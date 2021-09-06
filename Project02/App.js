603922cb729a83cd49487d1fed7178b929546c16
claudiodax <claudio.zavala@tecsup.edu.pe>
2021-09-06 16:06:35 -0500

    actualizacion

--- Project02/App.js
+++ Project02/App.js
@@ -30,21 +30,19 @@ onPress = () =>{
     return(
       <View style={styles.container}>
         <View style={styles.text}>
-          <Text>Hola Amigos</Text>
+          <Text style={styles.title}>ATARI GAMES</Text>
           </View>
 
-          <Image source={require('./img/react-logo2.png')}></Image>
-        <TextInput style={{height:40,borderColor:'gray',borderWidth:1}} 
+          <Image source={require('./img/Atari.png')} style={{width:460}}></Image>
+        <TextInput style={{height:40,borderColor:'lightgray',backgroundColor:'lightgray',borderWidth:1}} 
+        onChangeText={text => this.changeTextInput(text)} 
+        value={this.state.textValue}/>
+        <TextInput style={{height:40,borderColor:'gray',backgroundColor:'gray',borderWidth:1,marginVertical:5}} 
         onChangeText={text => this.changeTextInput(text)} 
         value={this.state.textValue}/>
         <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text> Touch Here</Text>
+          <Text> Login</Text>
         </TouchableOpacity>
-        <View style={[styles.countContainer]}>
-          <Text style={[styles.countText]}>
-            {this.state.count}
-          </Text>
-        </View>
       </View>
 
       
@@ -53,10 +51,17 @@ onPress = () =>{
 }
 
 const styles = StyleSheet.create({
+  title: {
+    color: 'white',
+    fontSize:40,
+    justifyContent: 'center',
+    alignItems:'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal:10,
+    backgroundColor: 'black',
   },
   text:{
     alignItems:'center',
@@ -65,7 +70,7 @@ const styles = StyleSheet.create({
   button:{
     top: 10,
     alignItems:'center',
-    backgroundColor:'#DDDDDD',
+    backgroundColor:'#bb3535',
     padding:10,
   },
   countContainer :{
